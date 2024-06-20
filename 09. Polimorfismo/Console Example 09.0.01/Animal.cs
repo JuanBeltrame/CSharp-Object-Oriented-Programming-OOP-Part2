e99a8b17659107bf2303d8898fa562525c576338
Juan Beltrame <juan_beltrame@hotmail.com>
2024-06-20 17:11:54 -0300

    Update Polimorfismo

---
+++ 09. Polimorfismo/Console Example 09.0.01/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Example_09._0._01
+{
+    // Clase creada dentro de otra clase, solo para que sea mas facil de visualizar. 
+    public class Animal
+    {
+        public virtual string EmitirSonido()
+        {
+            return "RoaaaaR";
+        }
+    }
+    public class Perro : Animal
+    {
+        public override string EmitirSonido()
+        {
+            return "woof!!!";
+        }
+    }
+    public class Cachorro : Perro
+    {
+    }
+    public class Carpincho : Animal
+    {
+        public override string EmitirSonido()
+        {
+            return "Viva la revolucion carpincha!";
+        }
+    }
+}
---
+++ 09. Polimorfismo/Console Example 09.0.01/Program.cs
@@ -0,0 +1,30 @@
+﻿using Console_Example_09._0._01;
+
+//Animal animal = new Animal(); 
+//Animal perro = new Perro();
+//Animal carpincho = new Carpincho(); 
+//Perro otroPerro = new Perro(); 
+
+//Console.WriteLine($" Animal: {animal.EmitirSonido()}");
+//Console.WriteLine($" Perro: {perro.EmitirSonido()}");
+//Console.WriteLine($" Carpincho: {carpincho.EmitirSonido()}");
+//Console.WriteLine($" otro Perro: {otroPerro.EmitirSonido()}");
+
+static void EscucharAnimal(Animal animal)
+{
+    Console.WriteLine($" Este animal hace... : {animal.EmitirSonido()}");
+}
+
+
+List<Animal> animales = new List<Animal>();
+animales.Add(new Animal()); // Roar
+animales.Add(new Perro()); // Woof
+animales.Add(new Carpincho()); // Viva la revolucion
+animales.Add(new Perro()); // Woof
+
+foreach (Animal animal in animales)
+{
+    EscucharAnimal(animal);
+}
+
+
