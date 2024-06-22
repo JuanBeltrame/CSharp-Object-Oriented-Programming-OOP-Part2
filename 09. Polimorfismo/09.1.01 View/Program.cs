8422d1ba424342a5007f4feb8980ee4f226ed273
Juan Beltrame <juan_beltrame@hotmail.com>
2024-06-22 16:39:05 -0300

    Update

---
+++ 09. Polimorfismo/09.1.01 View/Program.cs
@@ -0,0 +1,14 @@
+﻿using _09._1._01_View;
+
+Sobrescrito sobrecarga = new();
+
+Console.WriteLine(sobrecarga.ToString());
+
+string objeto = "Este es mi metodo ToString sobreescrito!";
+
+Console.WriteLine("---------------------------------------------");
+Console.Write("Comparacion Sobrecarga con String: ");
+Console.WriteLine(sobrecarga.Equals(objeto));
+
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine(sobrecarga.GetHashCode());
---
+++ 09. Polimorfismo/09.1.01 View/Sobrescrito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._1._01_View
+{
+    internal class Sobrescrito
+    {
+        public override string ToString()
+        {
+            return "Este es mi metodo ToString sobreescrito!";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.GetType() == typeof(Sobrescrito);
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510181;
+        }
+    }
+}
